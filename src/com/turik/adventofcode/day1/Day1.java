252c5826e98f9bcb45ecf4e6c466a6f185b985db
Ilia Turov <meetyourturik@gmail.com>
2022-12-02 12:01:09 -0500

    days 1 and 2

---
+++ src/com/turik/adventofcode/day1/Day1.java
@@ -0,0 +1,35 @@
+package com.turik.adventofcode.day1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Day1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("./inputs/day1.txt"));
+        String line;
+
+        int current = 0;
+        Queue<Integer> calories = new PriorityQueue<>(Comparator.naturalOrder());
+
+        while ((line = reader.readLine()) != null) {
+            if (line.isEmpty()) {
+                if (calories.isEmpty() || calories.size() < 3) {
+                    calories.offer(current);
+                } else if (current > calories.element()) {
+                    calories.poll();
+                    calories.offer(current);
+                }
+                current = 0;
+            } else {
+                current += Integer.parseInt(line);
+            }
+        }
+        System.out.println(calories);
+        System.out.println(calories.poll());
+        System.out.println(calories.poll());
+        System.out.println(calories.poll());
+    }
+}
---
+++ src/com/turik/adventofcode/day1/Day2.java
@@ -0,0 +1,94 @@
+package com.turik.adventofcode.day1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day2 {
+
+    enum RockPaperScissors {
+        ROCK(1, 2, 3),
+        PAPER(2, 3, 1),
+        SCISSORS(3, 1, 2);
+
+        public final int score; // how many points gives this choice
+        public final int winningScore; // how many points gives winning AGAINST this choice
+        public final int loosingScore; // how many points gives loosing AGAINST this choice
+
+        RockPaperScissors(int score, int winningScore, int loosingScore) {
+            this.score = score;
+            this.winningScore = winningScore;
+            this.loosingScore = loosingScore;
+        }
+    }
+
+    // used for part one of the problem
+    static class RPSComparator implements Comparator<RockPaperScissors> {
+
+        @Override
+        public int compare(RockPaperScissors o1, RockPaperScissors o2) {
+            if (o1 == o2) {
+                return 0;
+            }
+            if (o1 == RockPaperScissors.ROCK) {
+                return o2 == RockPaperScissors.SCISSORS ? 1 : -1;
+            } else if (o1 == RockPaperScissors.PAPER) {
+                return o2 == RockPaperScissors.ROCK ? 1 : -1;
+            } else { // SCISSORS
+                return o2 == RockPaperScissors.PAPER ? 1 : -1;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        RPSComparator comparator = new RPSComparator();
+
+        Map<String, RockPaperScissors> choiceMap = new HashMap<>();
+        choiceMap.put("A", RockPaperScissors.ROCK);
+        choiceMap.put("B", RockPaperScissors.PAPER);
+        choiceMap.put("C", RockPaperScissors.SCISSORS);
+        // for part 1
+        choiceMap.put("X", RockPaperScissors.ROCK);
+        choiceMap.put("Y", RockPaperScissors.PAPER);
+        choiceMap.put("Z", RockPaperScissors.SCISSORS);
+
+        int result1 = 0;
+        int result2 = 0;
+
+        BufferedReader reader = new BufferedReader(new FileReader("./inputs/day2.txt"));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] choices = line.split(" ");
+            RockPaperScissors opponent = choiceMap.get(choices[0]);
+            String strategy = choices[1];
+            RockPaperScissors you = choiceMap.get(strategy); // again, part 1
+
+            int comparingRes = comparator.compare(you, opponent);
+            result1 += you.score;
+
+            if (comparingRes > 0) { // win
+                result1 += 6;
+            } else if (comparingRes == 0) { // draw
+                result1 += 3;
+            } else { // lose
+                result1 += 0;
+            }
+
+            if (strategy.equals("Z")) { // win
+                result2 += 6;
+                result2 += opponent.winningScore;
+            } else if (strategy.equals("Y")) { // draw
+                result2 += 3;
+                result2 += opponent.score;
+            } else { // lose
+                result2 += opponent.loosingScore;
+            }
+        }
+
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+}
