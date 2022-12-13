ca825923bfecda897702b2c5300e345c9eb053b2
Ilia Turov <meetyourturik@gmail.com>
2022-12-12 22:23:13 -0500

    boilerplate

---
+++ src/com/turik/adventofcode/day13/Day13Part1.java
@@ -0,0 +1,17 @@
+package com.turik.adventofcode.day13;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Day13Part1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("./inputs/day13.txt"));
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+
+        }
+    }
+}
---
+++ src/com/turik/adventofcode/day15/Day15Part1.java
@@ -0,0 +1,17 @@
+package com.turik.adventofcode.day15;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Day15Part1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("./inputs/day15.txt"));
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+
+        }
+    }
+}
