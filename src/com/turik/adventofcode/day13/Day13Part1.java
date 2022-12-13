3447485dca0b5e9af47323c9e83d0fe8b816bf49
Ilia Turov <meetyourturik@gmail.com>
2022-12-13 17:10:16 -0500

    fixed parsing, comparison still wrong lol

--- src/com/turik/adventofcode/day13/Day13Part1.java
+++ src/com/turik/adventofcode/day13/Day13Part1.java
@@ -24,13 +24,10 @@ public class Day13Part1 {
                 }
             }
         }
-
         throw new RuntimeException("we were supposed to find a corresponding bracket!");
     }
 
     static List<Object> getPacketFromString(String line) {
-        // idk some implementation
-        // example line: [1,[2,[3,[4,[5,6,7]]]],8,9]
         List<Object> packet = new ArrayList<>();
 
         if (!line.contains(String.valueOf(open))) { // we assume that all brackets are properly paired
@@ -41,18 +38,13 @@ public class Day13Part1 {
             return packet;
         } else {
             int openPos = line.indexOf(open);
-            int prevClose = 0;
+            int prevClose = -2;
 
             while (openPos != -1) {
                 int closePos = getCorrespondingClosedBracket(line, openPos);
 
-                if (openPos > 0) {
-                    String numbersPart = line.substring(0, openPos - 1);
-                    packet.addAll(getPacketFromString(numbersPart));
-                }
-
                 if (openPos - prevClose > 2) { // there're some numbers between arrays
-                    String numbersPart = line.substring(prevClose + 1, openPos - 1);
+                    String numbersPart = line.substring(prevClose + 2, openPos - 1);
                     packet.addAll(getPacketFromString(numbersPart));
                 }
 
@@ -61,24 +53,54 @@ public class Day13Part1 {
 
                 openPos = line.indexOf(open, closePos);
 
-                if (closePos < line.length() - 1) { // there're some more numbers after our array
-                    int tmpEnd = openPos != -1 ? openPos : line.length();
-                    String numbersPart = line.substring(closePos + 1, tmpEnd);
-                    packet.addAll(getPacketFromString(numbersPart));
-                }
-
                 prevClose = closePos;
+            }
+
+            if (prevClose < line.length() - 1) { // there're some more numbers after our array
+                String numbersPart = line.substring(prevClose + 1);
+                packet.addAll(getPacketFromString(numbersPart));
             }
 
             return packet;
         }
     }
 
-    static int comparePair(Pair<List<Object>> pair) {
-        List<Object> left = pair.left();
-        List<Object> right = pair.right();
-        // again some impl that probably is recursive and returns 1 if left < right, -1 otherwise
-        return 1;
+    // 1 means right order, -1 - wrong
+    static int compareObjects(Object left, Object right) { // Object is either int or List<Integer>
+        if (left instanceof Integer && right instanceof Integer) {
+            if ((int) left == (int) right) {
+                return 0;
+            }
+            return (int) left < (int) right ? 1 : -1;
+        } else if (left instanceof List && right instanceof Integer) {
+            List<Object> tmpRight = new ArrayList<>();
+            tmpRight.add(right);
+            return compareObjects(left, tmpRight);
+        } else if (left instanceof Integer && right instanceof List) {
+            List<Object> tmpLeft = new ArrayList<>();
+            tmpLeft.add(left);
+            return compareObjects(tmpLeft, right);
+        } else if (left instanceof List && right instanceof List){ // both are lists
+            List<Object> leftList = (List<Object>) left;
+            List<Object> rightList = (List<Object>) right;
+
+            int minSize = Math.min(leftList.size(), rightList.size());
+
+            for (int i = 0; i < minSize; i++) {
+                int compare = compareObjects(leftList.get(i), rightList.get(i));
+                if (compare != 0) {
+                    return compare;
+                }
+            }
+
+            if (leftList.size() == rightList.size()) {
+                return 1;
+            }
+
+            return minSize == leftList.size() ? 1 : -1;
+        } else {
+            throw  new RuntimeException("should've processed all cases above");
+        }
     }
 
     public static void main(String[] args) throws IOException, ScriptException {
@@ -99,16 +121,13 @@ public class Day13Part1 {
                     getPacketFromString(s2.substring(s2.indexOf(open) + 1, s2.lastIndexOf(close)))
             );
 
-
-            System.out.println(pair.left() + " " + pair.right() + "\n");
-
             packets.add(pair);
         }
 
         int res = 0;
 
         for (int i = 0; i < packets.size(); i++) {
-            int comparison = comparePair(packets.get(i));
+            int comparison = compareObjects(packets.get(i).left(), packets.get(i).right());
             if (comparison == 1) {
                 res += i+1;
             }
