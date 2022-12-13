fb88879a37a394e585d0d7f97a3cc8c1a8e9ca70
Ilia Turov <meetyourturik@gmail.com>
2022-12-12 20:02:52 -0500

    made day 12 a bit more readable

--- src/com/turik/adventofcode/day12/Day12Part1And2.java
+++ src/com/turik/adventofcode/day12/Day12Part1And2.java
@@ -24,26 +24,20 @@ public class Day12Part1And2 {
     }
 
     static int getHeight(int x, int y) {
-        int height;
         if (x == start[0] && y == start[1]) {
-            height = 0;
+            return 0;
         } else if (x == end[0] && y == end[1]) {
-            height = 'z' - 'a';
+            return 'z' - 'a';
         } else {
-            height = strings.get(y).charAt(x) - 'a';
+            return strings.get(y).charAt(x) - 'a';
         }
-        return height;
     }
 
     public static void processNode(int[] coords, int distance) {
         int x = coords[0];
         int y = coords[1];
 
-        if (distance < distances[y][x]) {
-            distances[y][x] = distance;
-        } else {
-            return;
-        }
+        distances[y][x] = distance;
 
         if (x == end[0] && y == end[1]) {
             return;
@@ -59,13 +53,12 @@ public class Day12Part1And2 {
             int childy = y + ychanges[i];
             boolean inField = childx >= 0 && childx < maxX && childy >= 0 && childy < maxY;
             if (inField ) {
-                boolean isOptimalWay = distances[childy][childx] > distance;
+                boolean isOptimalWay = distances[childy][childx] > distance + 1; // if child distance is too low then why even bother going there
                 int childHeight = getHeight(childx, childy);
                 if (childHeight - height <= 1 && isOptimalWay) {
                     processNode(new int[]{childx, childy}, distance + 1);
                 }
             }
-
         }
     }
 
@@ -89,12 +82,10 @@ public class Day12Part1And2 {
                 end = new int[]{x, y};
             }
             if (line.contains("a")) {
-                int prevIdx = line.indexOf('a');
-                lowestPoints.add(new int[]{prevIdx, y});
-                int idx;
-                while ((idx = line.indexOf('a', prevIdx + 1)) != -1) {
-                    lowestPoints.add(new int[]{idx, y});
-                    prevIdx = idx;
+                for (int i = line.indexOf("a"); i < line.length(); i++) {
+                    if (line.charAt(i) == 'a') {
+                        lowestPoints.add(new int[]{i, y});
+                    }
                 }
             }
             strings.add(line);
@@ -105,15 +96,16 @@ public class Day12Part1And2 {
         maxX = strings.get(0).length();
         distances = new int[maxY][maxX];
 
-        resetDistances();
         // part 1
-        processNode(new int[]{start[0], start[1]}, 0);
+        resetDistances();
+        processNode(start, 0);
         System.out.printf("part 1 answer: %d\n", distances[end[1]][end[0]]);
+
         // part 2
         trails.add(distances[end[1]][end[0]]);
         for (int[] low: lowestPoints) {
             resetDistances();
-            processNode(new int[]{low[0], low[1]}, 0);
+            processNode(low, 0);
             trails.add(distances[end[1]][end[0]]);
         }
 
