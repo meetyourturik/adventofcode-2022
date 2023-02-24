7e662de5afddb8b9cdf0c8c729df51861eb71ff1
Ilia Turov <meetyourturik@gmail.com>
2023-02-24 10:32:56 -0500

    day 25

--- src/com/turik/adventofcode/day25/Day25Part1.java
+++ src/com/turik/adventofcode/day25/Day25Part1.java
@@ -5,12 +5,80 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Day25Part1 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("./inputs/day25t.txt"));
-        String line;
 
-        while ((line = reader.readLine()) != null) {
-
+    static int getNumFromChar(char c) {
+        switch (c) {
+            case '2':
+                return 2;
+            case '1':
+                return 1;
+            case '0':
+                return 0;
+            case '-':
+                return -1;
+            case '=':
+                return -2;
+            default:
+                throw new IllegalArgumentException("unknown character: " + c);
         }
+    }
+
+    static long snafuToDec(String snafu) {
+        long res = 0;
+        long power = 1;
+
+        for (int i = snafu.length() - 1; i >=0; i--) {
+            char c = snafu.charAt(i);
+            int num = getNumFromChar(c);
+            res += num * power;
+            power *= 5;
+        }
+
+        return res;
+    }
+
+    static char getCharFromNum(Long n) {
+        int num = n.intValue();
+        switch (num) {
+            case 2:
+                return '2';
+            case 1:
+                return '1';
+            case 0:
+                return '0';
+            case -1:
+                return '-';
+            case -2:
+                return '=';
+            default:
+                throw new IllegalArgumentException("wrong number: " + num);
+        }
+    }
+
+    static String decToSnafu(long num) {
+        StringBuilder sb = new StringBuilder();
+        long pow = 5;
+        while (num > 0) {
+            long r;
+            if (num % 5 > 2) {
+                r = (num % 5) - 5;
+                num +=5;
+            } else {
+                r = num % 5;
+            }
+            num /= 5;
+            sb.append(getCharFromNum(r));
+        }
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("./inputs/day25.txt"));
+        String line;
+        long res = 0;
+        while ((line = reader.readLine()) != null) {
+            res += snafuToDec(line);
+        }
+        System.out.println(decToSnafu(res));
     }
 }
